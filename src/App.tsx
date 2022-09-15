6d4f2c118ca25f6ac07b4b01e8f1a7613c98133c
Victor Olsson <victor.olsson@hiveandfive.se>
2022-09-15 21:07:20 +0200

    Basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,215 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div style={containerStyle}>
+      <div style={feedBox}>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+      </div>
+      <div style={feedBox}>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+      </div>
+      <div style={feedBox}>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+      </div>
+      <div style={feedBox}>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+      </div>
+      <div style={feedBox}>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+      </div>
+      <div style={feedBox}>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+      </div>
+      <div style={feedBox}>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+      </div>
+      <div style={feedBox}>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+        <div style={feedNews}>
+          <p>NEWS</p>
+        </div>
+      </div>
       
     </div>
   )
+}
+
+let containerStyle: React.CSSProperties = {
+  display: "flex",
+  height: "100vh",
+  backgroundColor: "lightblue",
+  gap: "10px",
+  padding: "10px",
+  overflowX: "auto"
+}
+
+let feedBox: React.CSSProperties = {
+  height: "100%",
+  minWidth: "200px",
+  backgroundColor: "lightcoral",
+  padding: "5px",
+  gap: "5px",
+  borderRadius: "5px",
+
+  display: "flex",
+  flexDirection: "column",
+  overflowY:"auto"
+}
+
+let feedNews: React.CSSProperties = {
+  minHeight: "100px",
+  backgroundColor: "lightgray",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  borderRadius: "5px"
 }
 
 export default App
