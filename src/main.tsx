e0350ee150d9a5a963aeddc69540fe189365b80e
Victor Olsson <victor.olsson@hiveandfive.se>
2022-10-11 13:34:42 +0200

    Fixed time-ago bug

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,10 @@ import './index.css'
 import TimeAgo from 'javascript-time-ago'
 
 import en from 'javascript-time-ago/locale/en.json'
-import ru from 'javascript-time-ago/locale/ru.json'
+
 
 TimeAgo.addDefaultLocale(en)
-TimeAgo.addLocale(ru)
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
